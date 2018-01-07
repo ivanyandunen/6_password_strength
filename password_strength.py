c2a0f74e5fdd78a8ee24f008834ae6fc74a6a6d8
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-08 00:01:38 +0300

    unnecessary bool removed; split() fixed;

--- password_strength.py
+++ password_strength.py
@@ -4,35 +4,35 @@ import getpass
 
 def load_blacklist(filepath):
     with open(filepath) as blacklist:
-        return blacklist.read()
+        return blacklist.read().split('\n')
 
 
-def password_not_in_blacklist(password, blacklist):
-    return bool(password not in blacklist.split())
+def password_in_blacklist(password, blacklist):
+    return password in blacklist
 
 
 def get_password_length(password):
     recommended_length = 8
-    return bool(len(password) >= recommended_length)
+    return len(password) >= recommended_length
 
 
 def password_contains_numbers(password):
-    return bool(any(char.isdigit() for char in password))
+    return any(char.isdigit() for char in password)
 
 
 def case_sensitivity_check(password):
     if not password.isdigit():
-        return bool(not(password.isupper() or password.islower()))
+        return not bool(password.isupper() or password.islower())
     else:
         return False
 
 
 def password_contains_special(password):
-    return bool(not password.isalnum())
+    return not password.isalnum()
 
 
 def get_password_strength(password, blacklist):
-    if not password_not_in_blacklist(password, blacklist):
+    if password_in_blacklist(password, blacklist):
         strength = 1
     else:
         strength = sum([
@@ -40,7 +40,7 @@ def get_password_strength(password, blacklist):
             password_contains_numbers(password) * 2,
             case_sensitivity_check(password) * 2,
             password_contains_special(password) * 2,
-            password_not_in_blacklist(password, blacklist) * 2
+            (not (password_in_blacklist(password, blacklist))) * 2
         ])
     return strength
 
@@ -56,5 +56,3 @@ if __name__ == '__main__':
         print('Input file is not specified or missed')
     except UnicodeDecodeError:
         print('Incorrect file')
-    except SystemExit:
-        print('Your password in blacklist! Strength = 1 Please try again')
